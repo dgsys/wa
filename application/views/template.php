6171e2a21b58b3fd2da59b47f0ca9e701d524162
denmasuciel <ekoprasetyo.cki09@gmail.com>
2019-11-30 12:02:23 +0700

    ubah template

--- application/views/template.php
+++ application/views/template.php
@@ -57,7 +57,7 @@
       <nav class="navbar navbar-static-top">
         <div class="container">
           <div class="navbar-header">
-            <a href="<?php echo base_url('Depan');?>" class="navbar-brand"><b>Pendaftaran</b> WA</a>
+            <a href="<?php echo base_url('Depan');?>" class="navbar-brand"><b><i class="fa fa-whatsapp">&nbsp;</i>Pendaftaran</b> WA</a>
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse">
               <i class="fa fa-bars"></i>
             </button>
